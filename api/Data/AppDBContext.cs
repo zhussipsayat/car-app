fadbfcd353f53fd3b383b6084cd0317a9ef8f933
Sayat Zhussip <sayat1997@gmail.com>
2024-02-19 13:18:17 +0600

    Changing comment sections

--- api/Data/AppDBContext.cs
+++ api/Data/AppDBContext.cs
@@ -14,7 +14,7 @@ namespace api.Data
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //Seeding State Master Data using HasData method
+            //Seeding HColor Data using HasData method
             modelBuilder.Entity<HColor>().HasData(
                 new() { Id = 1, Name = "Red" },
                 new() { Id = 2, Name = "Blue" },
@@ -22,7 +22,7 @@ namespace api.Data
                 new() { Id = 4, Name = "White" },
                 new() { Id = 5, Name = "Black" }
             );
-            //Seeding Country Master Data using HasData method
+            //Seeding Car Data using HasData method
             modelBuilder.Entity<Car>().HasData(
                new() { Id = 1, BrandName = "BWM", ModelName = "X5 AMG", HColorId = 5 },
                new() { Id = 2, BrandName = "Mercedes", ModelName = "525 E-class", HColorId = 2 }
